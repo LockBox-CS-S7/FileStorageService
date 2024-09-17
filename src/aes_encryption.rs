7e7e20752f81d8ff370505408adea282a5333422
rikdgd <74911706+rikdgd@users.noreply.github.com>
2024-09-17 14:04:30 +0200

    The encrypt function now uses user provided input.

--- src/aes_encryption.rs
+++ src/aes_encryption.rs
@@ -1,18 +1,14 @@
-use aes_gcm::{aead::{Aead, AeadCore, KeyInit, OsRng}, Aes256Gcm, Nonce, Key, AesGcm, aead};
-use aes_gcm::aead::consts::U12;
-use aes_gcm::aes::Aes256;
+use aes_gcm::{aead::{Aead, AeadCore, KeyInit, OsRng}, Aes256Gcm, Nonce, Key};
+
 
 fn encrypt(data: &[u8], key: &[u8; 32]) -> Result<EncryptedData, aes_gcm::Error> {
     // Transformed from a byte array:
-    let key: &[u8; 32] = &[42; 32];
     let key: &Key<Aes256Gcm> = key.into();
 
     let cipher = Aes256Gcm::new(&key);
-    let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
     
-    let ciphertext = cipher.encrypt(&nonce, b"plaintext message".as_ref())?;
-    // let plaintext = cipher.decrypt(&nonce, ciphertext.as_ref())?;
-    // assert_eq!(&plaintext, b"plaintext message");
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
+    let ciphertext = cipher.encrypt(&nonce, data)?;
     
     Ok(EncryptedData::new(&ciphertext, &nonce))
 }
@@ -22,7 +18,7 @@ fn decrypt(data: EncryptedData, key: &[u8; 32]) -> Result<Vec<u8>, aes_gcm::Erro
     let cipher = Aes256Gcm::new(&key);
     
     let nonce = Nonce::clone_from_slice(data.nonce.as_slice());
-    let plain_text = cipher.decrypt(&nonce, data.data().as_slice())?;
+    let plain_text = cipher.decrypt(&nonce, data.data.as_slice())?;
     
     Ok(plain_text)
 }
@@ -39,13 +35,5 @@ impl EncryptedData {
             data: data.to_vec(), 
             nonce: nonce.to_vec(),
         }
-    }
-
-    pub fn data(&self) -> &Vec<u8> {
-        &self.data
-    }
-
-    pub fn nonce(&self) -> &Vec<u8> {
-        &self.nonce
     }
 }
