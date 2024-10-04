1d925c2994d200e4c9d75184af5664eacd5bfab7
rikdgd <74911706+rikdgd@users.noreply.github.com>
2024-10-04 21:23:16 +0200

    fix errors

--- src/repository/file_repository.rs
+++ src/repository/file_repository.rs
@@ -1,20 +1,21 @@
-use std::io::Result;
+use std::io::Result as IoResult;
 use super::repository_base::RepositoryBase;
+use crate::models::FileModel;
 
 pub struct FileRepository {
     db_uri: String,
 }
 
-impl RepositoryBase for FileRepository {
-    fn get(&self, id: i32) -> IoResult<T> {
+impl RepositoryBase<FileModel> for FileRepository {
+    fn get(&self, id: i32) -> IoResult<FileModel> {
         todo!()
     }
 
-    fn create(&self, model: T) -> IoResult<i32> {
+    fn create(&self, model: FileModel) -> IoResult<i32> {
         todo!()
     }
 
-    fn update(&self, model: T) -> IoResult<()> {
+    fn update(&self, model: FileModel) -> IoResult<()> {
         todo!()
     }
 
